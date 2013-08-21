5459cd341ac89340c182064f9661169416dc4494
Marko Strukelj <marko.strukelj@gmail.com>
2013-08-21 23:17:05 +0200

    BZ 987708 Duplicate ResourceId at portlet war redeploy in development mode

--- component/web/resources/src/main/java/org/exoplatform/web/application/javascript/JavascriptConfigDeployer.java
+++ component/web/resources/src/main/java/org/exoplatform/web/application/javascript/JavascriptConfigDeployer.java
@@ -104,6 +104,13 @@ public class JavascriptConfigDeployer implements WebAppListener {
 
     private void remove(WebApp webApp) {
         javascriptService.unregisterServletContext(webApp);
+        try {
+            JavascriptConfigParser.unregisterResources(javascriptService, webApp.getServletContext());
+        } catch (Exception ex) {
+            LOG.error(
+                "An error occured while removing script resources for the context '"
+                    + webApp.getServletContext().getServletContextName() + "'", ex);
+        }
     }
 
     private void register(ServletContext scontext, PortalContainer container) {
